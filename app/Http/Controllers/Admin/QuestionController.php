1a16f66d471d79f46a8736973feae744dbad8ac0
Rafael <wolfninja355@gmail.com>
2023-12-16 19:49:19 -0500

    web cambio

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -9,9 +9,6 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $questions = Question::with('topic')
@@ -22,51 +19,31 @@ class QuestionController extends Controller
 
         return view('admin.questions.index', compact('questions'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         $topics = Topic::all();
         return view('admin.questions.create', compact('topics'));
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Question $question)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Question $question)
     {
         return view('admin.questions.edit', compact('question'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Question $question)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Question $question)
     {
         $question->delete();
