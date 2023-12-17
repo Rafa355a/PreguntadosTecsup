074b2b50e9e5bc555a471858fceea93078c134e0
Rafael <wolfninja355@gmail.com>
2023-12-16 19:52:22 -0500

    colores categoria

--- resources/views/admin/topics/index.blade.php
+++ resources/views/admin/topics/index.blade.php
@@ -3,7 +3,7 @@
     <h2>CATEGORIAS DISPONIBLES</h2>
     <hr>
     <div id="dashboard">
-        <div class="card gradiente3">
+        <div class="card gradiente3 celeste total">
             <span class="tema">Total</span>
             <span class="cantidad">
                 {{$topics->sum('questions_count')}}
@@ -14,7 +14,7 @@
         @foreach ($topics as $topic)
             <a href="{{route('admin.questions.index', [
                 'topic_id' => $topic->id,
-            ])}}" class="card gradiente1">
+            ])}}" class="card gradiente1 celeste">
                 <span class="tema">
                     {{ $topic->name }}
                 </span>
@@ -25,5 +25,21 @@
             </a>
         @endforeach
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Obtener el cuadro "Total" y asignar un color fijo
+            var totalCard = document.querySelector('.celeste.total');
+            totalCard.style.backgroundColor = '#66b2ff'; // Color celeste fijo
+
+            // Obtener los demás elementos celestes y asignar colores aleatorios
+            var celesteCards = document.querySelectorAll('.celeste:not(.total)');
+            celesteCards.forEach(function(card) {
+                // Generar un color aleatorio en formato hexadecimal
+                var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+                card.style.backgroundColor = randomColor;
+            });
+        });
+    </script>
 
 </x-admin-layout>
